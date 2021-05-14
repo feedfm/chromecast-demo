a8f13839ad765a3ed0948d4b31714fe92789ee98
Matt Sandler <mishugana@gmail.com>
2021-05-14 14:20:42 -0400

    Added some updates

--- receiver.js
+++ receiver.js
@@ -1,94 +1,102 @@
-const obe = {
-  events: {
-    live: {
-      chromecast_started: 'Stream Chromecast Started',
-      chromecast_stopped: 'Stream Chromecast Stopped',
-      paused: 'Stream Paused',
-      played: 'Stream Played by User',
-      progress: 'Stream Continued Watching',
-      started: 'Stream Started',
-      started_automatically: 'Stream Started Automatically',
-    },
-    vod: {
-      chromecast_started: 'Video Chromecast Started',
-      chromecast_stopped: 'Video Chromecast Stopped',
-      ended: 'Video Completed',
-      paused: 'Video Paused by User',
-      played: 'Video Played by User',
-      progress: 'Video in Progress',
-      seeked: 'Video Seek by User',
-      started: 'Video Started',
-      started_automatically: 'Video Started Automatically',
-      viewed: 'Viewed VOD',
-    }
-  }
-}
-
-var player = new Feed.Player('demo', 'demo', {"debug":true});
-player.on('all', function(event) {
-  console.log('player triggered event \'' + event + '\' with arguments:', Array.prototype.splice.call(arguments, 1));
-});
-player.setVolume(5);
-
 const TRACK_MEDIA_PROGRESS_INTERVAL = 45
 
 const context = cast.framework.CastReceiverContext.getInstance();
 
 const playerManager = context.getPlayerManager()
 
+var feedPlayer;
+
 let lastCurrentMediaTime = 0
 let config = { events: {} }
 
+function play_started(x){
+  let audio_file = x.audio_file;
+  let artist = audio_file.artist.name;
+  let release = audio_file.release.title;
+  let title = audio_file.track.title;
+  let feedInfo = document.getElementById("feed-info");    
+  let feedTitle = document.getElementById("title"); 
+  let feedArtist = document.getElementById("artist");
+  let feedRelease = document.getElementById("release");
+  feedTitle.innerHTML = title;
+  feedArtist.innerHTML = artist;
+  feedRelease.innerHTML = release;
+}
+
+
+
 const FEEDFM = 'urn:x-cast:fm.feed.cast';
-context.addCustomMessageListener(FEEDFM, function(customEvent) {
-  console.log(customEvent);
-  console.log("dkj0");
-  // handle customEvent.
+context.addCustomMessageListener(FEEDFM, event => {
+  if (event.data){
+    let data = event.data;
+    if (data.initialize)
+    {
+      var feedInitialize = data.initialize;
+      feedPlayer = new Feed.Player(feedInitialize.token, feedInitialize.secret, feedInitialize.options || {});
+      // feedPlayer.on('all', function(fEvent) {
+      //   let sendText = 'player triggered event \'' + fEvent + '\' with arguments:';
+      //   context.sendCustomMessage(FEEDFM, event.senderId ,sendText);
+      // });
+      
+      feedPlayer.on('play-started', play_started, play_started);  
+      feedPlayer.tune();
+    }
+    if (data.clear)
+    {
+      feedPlayer.session._deleteStoredCid();
+    }
+    if (data.volume)
+    {
+      feedPlayer.setVolume(data.volume);
+    }
+    if (data.play)
+    {
+      feedPlayer.play();
+    }
+    if (data.pause)
+    {
+      feedPlayer.pause();
+    }
+    if (data.stop)
+    {
+      feedPlayer.stop();
+    }
+    if (data.skip)
+    {
+      feedPlayer.skip();
+    }
+    if (data.station)
+    {
+      feedPlayer.setStationId(data.station);
+    }
+    if (data.stations)
+    {
+      let stationPromise = feedPlayer.getStations();
+      if (data.stations.name)
+      {
+        Promise.resolve(stationPromise).then(stations => {
+          let filteredStations = stations.filter(y=>y.name.includes(data.stations.name));
+          // Chromecast has a limit on their message sizes, so best to limit or filter on the receiver end before
+          // sending the message back
+          context.sendCustomMessage(FEEDFM, event.senderId ,{"stations":filteredStations.splice(0,100)});
+        }); 
+      }
+      else
+      {
+        Promise.resolve(stationPromise).then(stations => {
+          context.sendCustomMessage(FEEDFM, event.senderId ,{"stations":stations.splice(0,100)});
+        }); 
+      }  
+
+    }
+
+  }
+
+
 });
 
-playerManager.addEventListener(window.cast.framework.events.EventType.PLAYER_LOAD_COMPLETE, (event) => {
-  //play feed fm 
-  player.play();
-
-  const { media: { duration, metadata, customData } } = event
-  const { title: videoTitle } = metadata
-  const { userId, videoId, isLiveStream } = customData
-
-
-  console.log("hijijsi");
-
-  const media = {
-    live: {
-      events: obe.events.live,
-      tracking: { isCasting: true },
-    },
-    vod: {
-      events: obe.events.vod,
-      tracking: { isCasting: true, id: videoId, duration, videoTitle },
-    },
-  }
-
-  config = isLiveStream
-    ? media.live
-    : media.vod
-
-  window.analytics.identify(userId)
-})
-
-playerManager.addEventListener(window.cast.framework.events.EventType.TIME_UPDATE, (event) => {
-  if (event.currentMediaTime - lastCurrentMediaTime > TRACK_MEDIA_PROGRESS_INTERVAL) {
-    window.analytics.track(config.events.progress, { ...config.tracking })
-    lastCurrentMediaTime = event.currentMediaTime
-  }
-})
-
-playerManager.addEventListener(window.cast.framework.events.EventType.MEDIA_FINISHED, () => {
-  if (config.events.ended) {
-    window.analytics.track(config.events.ended, { ...config.tracking })
-  }
-  
-})
 
 
 
-context.start()
+const options = new cast.framework.CastReceiverOptions();
+context.start(options)
---
+++ sender/cast.js
@@ -0,0 +1,40 @@
+var castSession;
+var initializeCastApi = function() {
+  let applicationId = 'D527AC1B';
+  cast.framework.CastContext.getInstance().setOptions({
+    receiverApplicationId: applicationId,
+    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+  });
+  cast.framework.CastContext.getInstance().addEventListener('sessionstatechanged', startPlay);
+};
+var startPlay = function() {
+  castSession = cast.framework.CastContext.getInstance().getCurrentSession();
+
+  var currentMediaURL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+  var token = 'demo';
+  var secret = 'demo';
+  var stationName = "Station Two";
+  var contentType =  'video/mp4';
+
+  var stationId;
+  var mediaInfo = new chrome.cast.media.MediaInfo(currentMediaURL, contentType);
+  var request = new chrome.cast.media.LoadRequest(mediaInfo);
+  castSession.loadMedia(request).then(
+    function() { console.log('Load succeed'); },
+    function(errorCode) { console.log('Error code: ' + errorCode); });
+  castSession.addMessageListener('urn:x-cast:fm.feed.cast', (namespace, message) => {
+    jMessage = JSON.parse(message);
+    if (jMessage.stations){
+      stationId = jMessage.stations[0].id;
+    }
+    console.log(jMessage);
+  });
+
+  castSession.sendMessage('urn:x-cast:fm.feed.cast', {'initialize':{'token':token,'secret':secret}, 'volume':5});
+  castSession.sendMessage('urn:x-cast:fm.feed.cast', {'stations':{"name":stationName}});
+  castSession.sendMessage('urn:x-cast:fm.feed.cast', {'station':stationId});
+  castSession.sendMessage('urn:x-cast:fm.feed.cast', {'play':true});
+
+};
+
+
